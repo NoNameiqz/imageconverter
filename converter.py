c791c4d4cc45352e0fa8583267a471f19c5c24a2
NoNameiqz <71642601+NoNameiqz@users.noreply.github.com>
2026-04-17 19:10:14 +0100

    Refactor converter function to use PIL for image conversion

--- converter.py
+++ converter.py
@@ -1,24 +1,29 @@
-from wand.image import Image
+from PIL import Image
 
-def converter(output_type,input_name="",output_name=""):
+def converter(output_type, input_name="", output_name=""):
     if input_name == "":
         return False, "Empty file path"
+    
     try:
-        with Image(filename=input_name) as img:
-            img.format = output_type
-            if output_name == "":
-                output = f"coverted.{output_type}"
-            else:
-                output = output_name + "." + output_type
-            img.save(filename=output)
+        img = Image.open(input_name)
+
+        if output_type.lower() in ["jpg", "jpeg"]:
+            img = img.convert("RGB")
+
+        if output_name == "":
+            output = f"converted.{output_type}"
+        else:
+            output = output_name + "." + output_type
+
+        img.save(output, output_type.upper())
+
         return True, output
+
     except Exception as e:
-        return False, e
+        return False, str(e)
 
 
 
 
 #converter("png","resources/input.svg")
-
-
 
